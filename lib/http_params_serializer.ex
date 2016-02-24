719ba16f05253a7b2a6b222dbf4867fbfd3aae28
Roman Heinrich <roman.heinrich@gmail.com>
2016-02-24 11:56:27 +0100

    first viable implementation + tests

--- lib/http_params_serializer.ex
+++ lib/http_params_serializer.ex
@@ -1,2 +1,51 @@
 defmodule HttpParamsSerializer do
+  def normalize(params) when is_map(params) do
+    params |> convert_map_to_list |> normalize
+  end
+
+  def normalize(params) when is_list(params) do
+    normalize("", params, [])
+  end
+
+  def normalize(namespace, params = [{k, v}|t], acc) when is_list(v) do
+    cond do
+      Keyword.keyword?(v) ->
+        res = normalize(field_name(namespace, k), v, [])
+        normalize(namespace, t, res ++ acc  )
+      true -> # we have a normal list, special fieldname required!
+        res = Enum.map(v, fn(x)-> {field_name(:array, namespace, k), x} end)
+        normalize(namespace, t, res ++ acc )
+    end
+  end
+
+  def normalize(namespace, params = [{k, v}|t], acc) do
+    normalize(namespace, t, [ {field_name(namespace, k), v} | acc ])
+  end
+
+  # reverse + sorting only needed at the end of empty (== root) namespace
+  def normalize("", [], acc) do
+    acc |> Enum.reverse |> Enum.sort_by(fn({k,v})-> k end )
+  end
+  def normalize(namespace, [], acc),  do: acc
+
+  def field_name(namespace, field) do
+    case namespace do
+      "" -> "#{field}"
+      _  -> "#{namespace}[#{field}]"
+    end
+  end
+
+  def field_name(:array, namespace, field) do
+    case namespace do
+      "" -> "#{field}[]"
+      _  -> "#{namespace}[#{field}][]"
+    end
+  end
+
+  def convert_map_to_list(map) when is_map(map) do
+    map
+      |> Map.to_list
+      |> Enum.map( fn({k,v})-> {k, convert_map_to_list(v)} end)
+  end
+  def convert_map_to_list(map), do: map
 end
--- test/http_params_serializer_test.exs
+++ test/http_params_serializer_test.exs
@@ -1,8 +1,61 @@
 defmodule HttpParamsSerializerTest do
-  use ExUnit.Case
+  use ExSpec, async: true
   doctest HttpParamsSerializer
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe ":HttpParamsSerializer" do
+    def assert_output(i, o) do
+      assert HttpParamsSerializer.normalize(i) == o
+    end
+
+    describe ":normalize" do
+      it "accepts maps and converts them to keylists, so the result is same" do
+        output = [{"a", 1}, {"b", 2}]
+        assert_output %{a: 1, b: 2}, output
+        assert_output [a: 1, b: 2], output
+      end
+
+      it "supports nesting" do
+        assert_output %{a: %{b: 2}, c: 3},
+          [{"a[b]", 2}, {"c", 3}]
+      end
+
+      it "supports deep nesting (> 2)" do
+        assert_output %{a: %{b: %{d: [1,2], f: 4}}, c: 3},
+          [{"a[b][d][]", 2}, {"a[b][d][]", 1}, {"a[b][f]", 4}, {"c", 3}]
+      end
+
+      it "serializes lists in a special way" do
+        assert_output %{a: %{b: [1,2,3]}},
+          [{"a[b][]", 3}, {"a[b][]", 2}, {"a[b][]", 1}]
+      end
+
+      it "sorts the items alphabetically for predictability" do
+        input1 = %{z: 1, a: %{b: [1,2], c: 1} }
+        input2 = %{a: %{b: [1,2], c: 1}, z: 1}
+        input3 = %{a: %{c: 1, b: [1,2]}, z: 1}
+        output = [{"a[b][]", 2}, {"a[b][]", 1}, {"a[c]", 1}, {"z", 1}]
+        assert_output input1, output
+        assert_output input2, output
+        assert_output input3, output
+      end
+    end
+
+    describe ":convert_map_to_list" do
+      def assert_map_to_list(map, list) do
+        assert list == map |> HttpParamsSerializer.convert_map_to_list
+      end
+
+      it "converts maps to lists" do
+        assert_map_to_list(%{a: 1, b: 2}, [a: 1, b: 2])
+        assert_map_to_list(%{a: 1, b: 2, c: %{d: 4}}, [a: 1, b: 2, c: [d: 4]])
+        assert_map_to_list(%{a: 1, b: 2, c: %{d: 4, e: %{f: 6}}},
+            [a: 1, b: 2, c: [d: 4, e: [f: 6]]])
+      end
+
+      it "does not retain order (we can't control the order of keys in maps!)" do
+        assert_map_to_list(%{z: 1, a: %{b: [1,2], c: 1} },
+            [a: [b: [1, 2], c: 1], z: 1] )
+      end
+    end
   end
 end
